1f0f50e7b764d9cc629aa54b2c4fb3a10d23e8fb
Refayth <refayth.codemoly@gmail.com>
2025-07-06 16:01:01 +0600

    add: env

---
+++ env.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(str(BASE_DIR / ".env"))
+
+def get_env_variable(var_name):
+    value = os.getenv(var_name)
+    if value is None or value == "":
+        raise RuntimeError(f"Environment variable '{var_name}' is not set or empty.")
+    return value
